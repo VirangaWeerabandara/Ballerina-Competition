83f010ceaded69677e62468659961a8d0aef35c7
Layanga-Rajapakshe <dimutech46@gmail.com>
2025-07-20 14:51:03 +0530

    FInished layout

---
+++ frontend/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/layout";
+
+export default function ProjectsPage() {
+    return(
+        <Layout>
+            <h1>Your page content goes here</h1>
+        </Layout>
+    );
+}
